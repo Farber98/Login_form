8c7d8902a06c94bac6146cba5d22b42f1ca142a8
Juan Farber <juantuc98@gmail.com>
2019-07-26 01:47:59 -0300

    Creamos la estructura basica en html y establecemos estilos con CSS.

---
+++ contenido.php
@@ -0,0 +1,8 @@
+<?php   
+//Logica. Se encarga de comprobar que el usuario tenga una sesion.
+//En caso de no tenerla, mandara al usuario a iniciar sesion o a que se registre.
+//Si en verdad tiene una sesion, se cargara el contenido.view.php  
+ 
+ 
+ require 'views/contenido.view.php'
+ ?>
---
+++ views/contenido.view.php
@@ -0,0 +1,25 @@
+<?php 
+//Contenido habilitado solo para usuarios registrados.
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Contenido</title>
+</head>
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Contenido del sitio</h1>
+        <a href="cerrar.php">Cerrar sesion</a>
+        <div class="contenido">
+            <article>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, facere repellat. Commodi eos provident vel ab. Voluptates illum ullam culpa fugiat et suscipit dignissimos repudiandae sit eum ad, totam ipsa.</p>
+            </article>
+        </div>    
+    </div>
+</body>
+</html>
