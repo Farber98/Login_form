f3ec062ad2014b7cd795142d85102cbe4d5cb2fb
Juan Farber <juantuc98@gmail.com>
2019-08-01 16:38:13 -0300

    Creamos la parte visual de nuestro registro con sus respectivos estilos. Agregamos un boton con codigo JS para poder enviar el form al presionar y tambien para poder enviarlo con el enter. Se usaron iconos de FortAwesome.

--- views/registrate.view.php
+++ views/registrate.view.php
@@ -16,8 +16,30 @@
 <body>
     <div class="contenedor">
         <h1 class="titulo">Registrate</h1>
-        <hr class="border">
-        
-    </div>
+        <hr class="border"> <!-- Creamos el formulario de registro -->
+<!-- Recordar usar funcion htmlspecialchars para evitar la inyeccion de codigo. -->
+
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="POST" class="formulario" name="login">  <!-- Name para usar javascript -->
+            <div class="form-group">
+                <i class="icono izquierda fa fa-user"></i><input type="text" name="usuario" class="usuario" placeholder="Usuario"> <!-- fa fa-user es la clase que debemos usar para traer el icono de FortAwesome de usuario. Deben estar al lado las declaraciones para que quede como lo planeado esteticamente. -->
+            </div>
+
+            <div class="form-group">
+                <i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password" placeholder="Contraseña">  <!-- fa fa-lock es el candado-->
+            </div>
+
+            <div class="form-group">
+                <i class="icono izquierda fa fa-lock"></i><input type="password" name="password2" class="password_btn" placeholder="Repetir contraseña">  <!-- fa fa-arrow es la flecha-->
+                <i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i> <!-- Login es el name del formulario. Lo actualizamos cuando se clickea el boton con codigo JS. -->
+            </div>
+
+            <div class="hidden-submit"><input type="submit" tabindex="-1"/></div> <!-- Para poder enviar con el enter. -->
+        </form>
+
+        <p class="texto-registrate">
+            ¿Ya tienes cuenta?
+            <a href="login.php">Iniciar sesion</a>
+        </p>
+   </div> 
 </body>
 </html>
