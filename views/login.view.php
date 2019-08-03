e80101a4cd2758bec91126d7893362515c3c3c19
Juan Farber <juantuc98@gmail.com>
2019-08-03 15:32:43 -0300

    Reutilizamos el registrate.view.php para hacer el login.view.php eliminando el campo de repetir contraseña y invitando a registrarse en caso de no tener cuenta. En las proximas versiones realizaremos la logica de validacion de los formularios y ademas la conexion para realizar consultas en nuestra base de datos

--- views/login.view.php
+++ views/login.view.php
@@ -1,7 +1,7 @@
-<?php 
-//Reutilizamos el diseño de contenido y cambiamos lo ncesario para hacer el registro.
-//Se encarga de darle el diseño de la vista al formulario de registro.
-//Invitamos al usuario a inciar sesion en caso de tener cuenta.
+<?php //Reutilizamos el archivo de registrate.php y sacamos el 
+//campo de repetir contraseña modificando el name y placeholder y 
+//eliminando el primer campo de contraseña asi queda con el diseño de la flechita para hacer el submit. 
+//Inivtamos al usuario a registrarse en caso de no tener cuenta.
 ?>
 
 <!DOCTYPE html>
@@ -12,12 +12,12 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400&display=swap" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilos.css">
-    <title>Registrate</title>
+    <title>Iniciar sesion</title>
 </head>
 <body>
     <div class="contenedor">
-        <h1 class="titulo">Registrate</h1>
-        <hr class="border"> <!-- Creamos el formulario de registro -->
+        <h1 class="titulo">Iniciar sesion</h1>
+        <hr class="border"> 
 <!-- Recordar usar funcion htmlspecialchars para evitar la inyeccion de codigo. -->
 
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="POST" class="formulario" name="login">  <!-- Name para usar javascript -->
@@ -26,11 +26,7 @@
             </div>
 
             <div class="form-group">
-                <i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password" placeholder="Contraseña">  <!-- fa fa-lock es el candado-->
-            </div>
-
-            <div class="form-group">
-                <i class="icono izquierda fa fa-lock"></i><input type="password" name="password2" class="password_btn" placeholder="Repetir contraseña">  <!-- fa fa-arrow es la flecha-->
+                <i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password_btn" placeholder="Contraseña">  <!-- fa fa-arrow es la flecha-->
                 <i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i> <!-- Login es el name del formulario. Lo actualizamos cuando se clickea el boton con codigo JS. -->
             </div>
 
@@ -38,8 +34,8 @@
         </form>
 
         <p class="texto-registrate">
-            ¿Ya tienes cuenta?
-            <a href="login.php">Iniciar sesion</a>
+            ¿Aun no tienes cuenta?
+            <a href="registrate.php">Registrate</a>     <!-- Si el usuario no tiene cuenta lo invitamos a registrarse. -->
         </p>
    </div> 
 </body>
